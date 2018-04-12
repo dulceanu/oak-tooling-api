1a41bdb40288d6ac77c14b47252704c556ddfce0
Andrei Dulceanu <dulceanu@adobe.com>
2018-04-12 14:07:59 +0300

    Analyze content growth per ROI Return stream of changes containing (path, changeType, changeContent) between two revisions

--- oak-tooling-it/src/test/java/org/apache/jackrabbit/oak/tooling/filestore/bindings/nodestate/ExampleQueries.java
+++ oak-tooling-it/src/test/java/org/apache/jackrabbit/oak/tooling/filestore/bindings/nodestate/ExampleQueries.java
@@ -18,25 +18,37 @@
 
 package org.apache.jackrabbit.oak.tooling.filestore.bindings.nodestate;
 
+import static com.google.common.collect.Iterables.transform;
+import static org.apache.commons.io.FileUtils.byteCountToDisplaySize;
+import static org.apache.jackrabbit.oak.api.Type.BINARIES;
+import static org.apache.jackrabbit.oak.api.Type.BINARY;
+import static org.apache.jackrabbit.oak.api.Type.STRING;
+import static org.apache.jackrabbit.oak.api.Type.STRINGS;
+import static org.apache.jackrabbit.oak.commons.PathUtils.concat;
 import static org.apache.jackrabbit.oak.tooling.filestore.api.Record.Type.NODE;
 import static org.apache.jackrabbit.oak.tooling.filestore.api.Segment.Type.DATA;
 import static org.apache.jackrabbit.oak.tooling.filestore.bindings.nodestate.NodeStateBackedSegmentStore.newSegmentStore;
 import static org.apache.jackrabbit.oak.tooling.filestore.bindings.nodestate.Streams.asStream;
+import static org.apache.jackrabbit.oak.plugins.memory.EmptyNodeState.EMPTY_NODE;
+import static org.apache.jackrabbit.oak.plugins.memory.EmptyNodeState.MISSING_NODE;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import com.microsoft.azure.storage.StorageException;
+import org.apache.jackrabbit.oak.api.Blob;
+import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.segment.file.FileStoreBuilder;
 import org.apache.jackrabbit.oak.segment.file.InvalidFileStoreVersionException;
 import org.apache.jackrabbit.oak.segment.file.ReadOnlyFileStore;
 import org.apache.jackrabbit.oak.segment.file.proc.Proc;
 import org.apache.jackrabbit.oak.spi.state.NodeState;
+import org.apache.jackrabbit.oak.spi.state.NodeStateDiff;
 import org.apache.jackrabbit.oak.tooling.filestore.api.JournalEntry;
 import org.apache.jackrabbit.oak.tooling.filestore.api.Record;
 import org.apache.jackrabbit.oak.tooling.filestore.api.Segment;
@@ -45,6 +57,11 @@ import org.apache.jackrabbit.oak.tooling.filestore.api.Tar;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.google.common.base.Function;
+import com.microsoft.azure.storage.StorageException;
+
+import one.util.streamex.StreamEx;
 
 public class ExampleQueries {
 
@@ -131,4 +148,165 @@ public class ExampleQueries {
         nodes.forEach(System.out::println);
     }
 
+    static enum ChangeType {
+    	PROPERTY_ADDED,
+    	PROPERTY_CHANGED,
+    	PROPERTY_DELETED,
+    	CHILD_NODE_ADDED,
+    	CHILD_NODE_CHANGED,
+    	CHILD_NODE_DELETED
+    }
+    
+    static class Change {
+    	private String path;
+    	private ChangeType type;
+    	private StringBuilder content;
+    	private long size;
+    	
+    	public Change(String path, ChangeType type, StringBuilder content, long size) {
+    		this.path = path;
+    		this.type = type;
+    		this.content = content;
+    		this.size = size;
+    	}
+
+		@Override
+		public String toString() {
+			return "Change [path=" + path + ", type=" + type + ", content=" + content + ", size=" + size + "]";
+		}
+    }
+    
+    static class RecordChangesDiff implements NodeStateDiff {
+		private static final Function<Blob, String> BLOB_LENGTH = new Function<Blob, String>() {
+
+			@Override
+			public String apply(Blob b) {
+				return safeGetLength(b);
+			}
+
+			private String safeGetLength(Blob b) {
+				try {
+					return byteCountToDisplaySize(b.length());
+				} catch (IllegalStateException e) {
+					// missing BlobStore probably
+				}
+				return "[N/A]";
+			}
+
+		};
+		
+    	private final List<Change> changes;
+    	private final String path;
+    	
+    	public RecordChangesDiff(String path, List<Change> changes) {
+    		this.path = path;
+    		this.changes = changes;
+    	}
+    	
+		@Override
+		public boolean propertyAdded(PropertyState after) {
+			StringBuilder content = new StringBuilder();
+			content.append("    + " + toString(after));
+			changes.add(new Change(path, ChangeType.PROPERTY_ADDED, content, 0));
+			return true;
+		}
+
+		@Override
+		public boolean propertyChanged(PropertyState before, PropertyState after) {
+			StringBuilder content = new StringBuilder();
+			content.append("    ^ " + before.getName() + "\n");
+            content.append("      - " + toString(before) + "\n");
+            content.append("      + " + toString(after));
+            
+            changes.add(new Change(path, ChangeType.PROPERTY_CHANGED, content, 0));
+            
+			return true;
+		}
+
+		@Override
+		public boolean propertyDeleted(PropertyState before) {
+			StringBuilder content = new StringBuilder();
+			content.append("    - " + toString(before));
+			changes.add(new Change(path, ChangeType.PROPERTY_DELETED, content, 0));
+			return true;
+		}
+
+		@Override
+		public boolean childNodeAdded(String name, NodeState after) {
+			String p = concat(path, name);
+			StringBuilder content = new StringBuilder();
+			content.append("+ " + p);
+			changes.add(new Change(p, ChangeType.CHILD_NODE_ADDED, content, 0));
+			RecordChangesDiff diff = new RecordChangesDiff(p, new ArrayList<>());
+			boolean val = after.compareAgainstBaseState(EMPTY_NODE, diff);
+			diff.changes().forEach(c -> changes.add(c));
+			return val;
+	    }
+
+		@Override
+		public boolean childNodeChanged(String name, NodeState before, NodeState after) {
+	        String p = concat(path, name);
+			StringBuilder content = new StringBuilder();
+			content.append("^ " + p);
+			changes.add(new Change(p, ChangeType.CHILD_NODE_CHANGED, content, 0));
+			RecordChangesDiff diff = new RecordChangesDiff(p, new ArrayList<>());
+			boolean val = after.compareAgainstBaseState(before, diff);
+			diff.changes().forEach(c -> changes.add(c));
+			return val;
+		}
+
+		@Override
+		public boolean childNodeDeleted(String name, NodeState before) {
+			String p = concat(path, name);
+			StringBuilder content = new StringBuilder();
+			content.append("- " + p);
+			changes.add(new Change(p, ChangeType.CHILD_NODE_CHANGED, content, 0));
+			RecordChangesDiff diff = new RecordChangesDiff(p, new ArrayList<>());
+			boolean val = MISSING_NODE.compareAgainstBaseState(before, diff);
+			diff.changes().forEach(c -> changes.add(c));
+			return val;
+		}
+		
+		public Iterable<Change> changes() {
+			return changes;
+		}
+		
+		private static String toString(PropertyState ps) {
+	        StringBuilder val = new StringBuilder();
+	        val.append(ps.getName()).append("<").append(ps.getType()).append(">");
+	        if (ps.getType() == BINARY) {
+	            String v = BLOB_LENGTH.apply(ps.getValue(BINARY));
+	            val.append(" = {").append(v).append("}");
+	        } else if (ps.getType() == BINARIES) {
+	            String v = transform(ps.getValue(BINARIES), BLOB_LENGTH).toString();
+	            val.append("[").append(ps.count()).append("] = ").append(v);
+	        } else if (ps.isArray()) {
+	            val.append("[").append(ps.count()).append("] = ").append(ps.getValue(STRINGS));
+	        } else {
+	            val.append(" = ").append(ps.getValue(STRING));
+	        }
+	        return ps.getName() + "<" + ps.getType() + ">" + val.toString();
+	    }
+    }
+    
+    @Test
+	public void analyzeContentGrowth() {
+		String path = "content";
+		String rev1 = "a0cf52ea-5858-427d-ab63-7a623fc88180:1886";
+		String rev2 = "3e22c18c-d29f-4348-aad5-8a47678ee66c:2415";
+		Stream<Change> changes = StreamEx.of(asStream(segmentStore.journalEntries())
+				.filter(j -> (j.segmentId()+":"+j.recordNumber()).equals(rev1)
+							|| (j.segmentId()+":"+j.recordNumber()).equals(rev2))
+				.map(JournalEntry::getRoot)
+				.map(node -> node.getChildNode("root").getChildNode(path)))
+				.pairMap((a,b) -> {
+					RecordChangesDiff diff = new RecordChangesDiff(path, new ArrayList<>());
+					a.compareAgainstBaseState(b, diff);
+					return diff;
+				})
+				.flatMap(asStream(RecordChangesDiff::changes))
+				.filter(c -> c.type == ChangeType.PROPERTY_ADDED);
+		changes.forEach(System.out::println);
+//		System.out.println("Changes:"  + changes.count());
+	}
 }
